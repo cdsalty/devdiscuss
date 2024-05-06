fab982f3b0c945fc56b7284fc6dcbbced1b5e97c
Christopher <soltiscd@gmail.com>
2024-05-06 16:18:29 -0400

    tinyMCE Editor

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import React, { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { useForm } from 'react-hook-form';
@@ -17,6 +19,9 @@ import { Input } from '../ui/input';
 import { QuestionsSchema } from '@/lib/validations';
 
 const Question = () => {
+  // Establish a reference to the editor.
+  const editorRef = useRef(null); // This is so you don't have to manually track every keystroke. You can just extract the value from it later on.
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
@@ -74,11 +79,42 @@ const Question = () => {
                 <span className="text-red-500">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                {/* TODO: Replace Input with WYSIWYG Editor */}
-                {/* <Input
-                  className="no-focus paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                  {...field}
-                /> */}
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  // @ts-ignore
+                  onInit={(_evt, editor) => (editorRef.current = editor)}
+                  initialValue=""
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      'advlist',
+                      'autolink',
+                      'lists',
+                      'link',
+                      'image',
+                      'charmap',
+                      'preview',
+                      'anchor',
+                      'searchreplace',
+                      'visualblocks',
+                      'codesample',
+                      'fullscreen',
+                      'insertdatetime',
+                      'media',
+                      'table',
+                      'code',
+                    ],
+                    toolbar:
+                      'undo redo | ' +
+                      'codesample | bold italic forecolor | alignleft aligncenter |' +
+                      'alignright alignjustify | bullist numlist',
+                    // content_style: 'body { font-family:Inter; font-size:16px }',
+                    // skin: mode === 'dark' ? 'oxide-dark' : 'oxide',
+                    // content_css: mode === 'dark' ? 'dark' : 'light',
+                    // content_style:'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+                  }}
+                />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Introduce the problem and expand on the title. Minimum of 20
@@ -114,7 +150,12 @@ const Question = () => {
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button
+          type="submit"
+          className="primary-gradient w-fit !text-light-900"
+        >
+          Submit
+        </Button>
       </form>
     </Form>
   );
